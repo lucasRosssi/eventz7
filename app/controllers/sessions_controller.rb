23db8b8b5cba2690a46e30f0678dee91f18f0952
Lucas Rossi <lucasrossi@fetch.ly>
2025-02-04 19:48:55 -0300

    add sign out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,7 @@ class SessionsController < ApplicationController
   end
 
   def destroy
-    
+    session[:user_id] = nil
+    redirect_to events_url, status: :see_other, notice: "You're now signed out!"
   end
 end
