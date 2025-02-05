18a94a837275758a060690eac101237d0f6270b6
Lucas Rossi <lucasrossi@fetch.ly>
2025-02-05 07:47:15 -0300

    add gatekeeper

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,15 @@ class ApplicationController < ActionController::Base
 
     helper_method :current_user
 
+    def current_user?(user)
+      current_user == user
+    end
+
+    helper_method :current_user?
+
     def require_signin
       unless current_user
+        session[:intended_url] = request.url
         redirect_to new_session_url, alert: "Please sign in first."
       end
     end
