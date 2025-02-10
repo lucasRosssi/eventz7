9d7c1abdd7283ee353b2a789c3ce07ea8d1182cc
Lucas Rossi <lucasrossi@fetch.ly>
2025-02-10 08:16:50 -0300

    add user-registration relation

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -9,9 +9,8 @@ class Registration < ApplicationRecord
   ]
 
   belongs_to :event
+  belongs_to :user
 
-  validates :name, presence: true
-  validates :email, format: { with: /\S+@\S+/ }
   validates :how_heard, inclusion: { in: HOW_HEARD_OPTIONS }
   
 end
