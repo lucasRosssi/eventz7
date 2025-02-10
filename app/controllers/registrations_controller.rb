d16cffb0a24539b6679108c70e0ece00c13ed0a6
Lucas Rossi <lucasrossi@fetch.ly>
2025-02-10 08:36:05 -0300

    updates for new relation

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,7 @@
 class RegistrationsController < ApplicationController
+  before_action :require_signin
   before_action :set_event
+
   def index
     @registrations = @event.registrations
   end
@@ -10,6 +12,7 @@ class RegistrationsController < ApplicationController
 
   def create
     @registration = @event.registrations.new(registration_params)
+    @registration.user = current_user
 
     if @registration.save
       redirect_to event_registrations_url(@event),
@@ -22,8 +25,6 @@ class RegistrationsController < ApplicationController
   private
     def registration_params
       params.require(:registration).permit(
-        :name,
-        :email,
         :how_heard
       )
     end
