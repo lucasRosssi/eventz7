98ee052d1e8ffd2647264b9441995b2afe83c07e
Lucas Rossi <lucasrossi@fetch.ly>
2025-02-19 08:19:27 -0300

    add filtered events

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :categories
   root "events#index"
+
+  get "events/filter/:filter" => "events#index", as: :filtered_events
   
   resources :events do
     resources :registrations
