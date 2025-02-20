e7031a707c41056b705fb589f8f8fb97238d8421
Lucas Rossi <lucasrossi@fetch.ly>
2025-02-20 07:51:10 -0300

    add slug

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_02_16_134109) do
+ActiveRecord::Schema[7.1].define(version: 2025_02_19_115526) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -36,6 +36,7 @@ ActiveRecord::Schema[7.1].define(version: 2025_02_16_134109) do
     t.text "description"
     t.string "image_file_name", default: "placeholder.png"
     t.integer "capacity", default: 1
+    t.string "slug"
   end
 
   create_table "likes", force: :cascade do |t|
